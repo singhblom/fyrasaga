5521f27a68c7dac73cb9429b1191a02a6cf6b309
Martin Singh-Blom <martin.blom@ki.se>
2013-05-28 19:40:01 +0200

    Made it classier.

--- charmake.dart
+++ charmake.dart
@@ -1,13 +1,41 @@
 import 'dart:html';
 import 'package:web_ui/web_ui.dart';
 
-//@observable
-var characterName = "Name McNamesson";
+
+class Character {
+  Map characteristics = {"Intelligence":0,"Perception":0,"Strength":0,"Stamina":0,"Presence":0,
+                         "Communication":0,"Quickness":0,"Dexterity":0};
+  int age = 0;
+  int year = 0;
+  bool magus = false;
+  String name;
+  Map virtuesAndFlaws = {};
+  Map skills = {};
+  Map relationships = {};
+  Map arts = {};
+  List personality = [];
+  List belongings = [];
+  Map chronicle = {};
+  
+  Character(this.name);
+  
+  Character.fromJson(Map json) {
+    characteristics = json['Characteristics'];
+    age = json['Age'];
+    year = json['Year'];
+    magus = json['Magus'];
+    virtuesAndFlaws = json['Virtues and flaws'];
+    skills = json['Skills'];
+    relationships = json['Relationships'];
+    arts = json['Arts'];
+    personality = json['Personality'];
+    belongings = json['Belongings'];
+    chronicle = json['Chronicle'];
+  }
+}
 
 
-// initial value for click-counter
-var characteristics = {"Intelligence":0,"Perception":0,"Strength":0,"Stamina":0,"Presence":0,
-                       "Communication":0,"Quickness":0,"Dexterity":0};
+var c = new Character("Name McNamesson");
 
 int pyramid(number) {
   if (number > 0) return (number*(number+1)/2).round();
@@ -16,34 +44,31 @@ int pyramid(number) {
 
 int totalCost() {
   int tot = 0;
-  characteristics.forEach((k,v) => tot = tot+pyramid(v));
+  c.characteristics.forEach((k,v) => tot = tot+pyramid(v));
   return tot;
 }
-
-var virtuesAndFlaws = [];
 
 void addVirtueOrFlaw(Event e) {
   e.preventDefault(); // don't submit the form
   var input = query('#new_virtue_or_flaw');
   if (input.value == '') return;
-  virtuesAndFlaws.add(input.value);
+  c.virtuesAndFlaws[input.value]="";
   input.value = '';
 }
 
-var skills = {};
 
 void addSkill(Event e) {
   e.preventDefault(); // don't submit the form
   var input = query('#new_skill');
   if (input.value == '') return;
-  skills[input.value]=0;
+  c.skills[input.value]=0;
   input.value = '';
 }
 
 
 int totalSkillCost() {
   int tot = 0;
-  skills.forEach((k,v) => tot = tot+5*pyramid(v));
+  c.skills.forEach((k,v) => tot = tot+5*pyramid(v));
   return tot;
 }
 
