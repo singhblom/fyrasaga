53f7ed041382bb527474cef9f2a794240d5d4512
Martin Singh-Blom <martin.blom@ki.se>
2013-05-29 08:27:58 +0200

    Added all of the attributes for a character. Got responsive css to work.

--- charmake.dart
+++ charmake.dart
@@ -1,12 +1,13 @@
 import 'dart:html';
 import 'package:web_ui/web_ui.dart';
+import 'dart:json' as json;
 
 
 class Character {
   Map characteristics = {"Intelligence":0,"Perception":0,"Strength":0,"Stamina":0,"Presence":0,
                          "Communication":0,"Quickness":0,"Dexterity":0};
-  int age = 0;
-  int year = 0;
+  String sAge = '0';
+  String sYear = '0';
   bool magus = false;
   String name;
   Map virtuesAndFlaws = {};
@@ -19,23 +20,40 @@ class Character {
   
   Character(this.name);
   
-  Character.fromJson(Map json) {
-    characteristics = json['Characteristics'];
-    age = json['Age'];
-    year = json['Year'];
-    magus = json['Magus'];
-    virtuesAndFlaws = json['Virtues and flaws'];
-    skills = json['Skills'];
-    relationships = json['Relationships'];
-    arts = json['Arts'];
-    personality = json['Personality'];
-    belongings = json['Belongings'];
-    chronicle = json['Chronicle'];
+  Character.fromJson(Map input) {
+    characteristics = input['Characteristics'];
+    sAge = input['Age'].toString();
+    sYear = input['Year'].toString();
+    magus = input['Magus'];
+    virtuesAndFlaws = input['Virtues and flaws'];
+    skills = input['Skills'];
+    relationships = input['Relationships'];
+    arts = input['Arts'];
+    personality = input['Personality'];
+    belongings = input['Belongings'];
+    chronicle = input['Chronicle'];
   }
+  
+  String stringify() {
+    Map output = {};
+    output['Characteristics'] = characteristics;
+    output['Age'] = this.age;
+    output['Year'] = this.year;
+    output['Magus'] = magus;
+    output['Virtues and flaws'] = virtuesAndFlaws;
+    output['Skills'] = skills;
+    output['Relationships'] = relationships;
+    output['Arts'] = arts;
+    output['Personality'] = personality;
+    output['Belongings'] = belongings;
+    output['Chronicle'] = chronicle;
+    return json.stringify(output);
+  }
+  int get age => int.parse(sAge);
+  int get year => int.parse(sYear);
 }
 
-
-var c = new Character("Name McNamesson");
+var c = new Character("Outis");
 
 int pyramid(number) {
   if (number > 0) return (number*(number+1)/2).round();
@@ -48,20 +66,11 @@ int totalCost() {
   return tot;
 }
 
-void addVirtueOrFlaw(Event e) {
+void addDescription(Event e, Map desc, String field, var init) {
   e.preventDefault(); // don't submit the form
-  var input = query('#new_virtue_or_flaw');
+  var input = query(field);
   if (input.value == '') return;
-  c.virtuesAndFlaws[input.value]="";
-  input.value = '';
-}
-
-
-void addSkill(Event e) {
-  e.preventDefault(); // don't submit the form
-  var input = query('#new_skill');
-  if (input.value == '') return;
-  c.skills[input.value]=0;
+  desc[input.value]=init;
   input.value = '';
 }
 
